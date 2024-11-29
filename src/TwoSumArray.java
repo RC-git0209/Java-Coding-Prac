5b1ed997c6c7970a6d4dca9ee11f1ba0e567aad6
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-11-29 15:26:47 +0530

    Initial commit for Two Sum Array

---
+++ src/TwoSumArray.java
@@ -0,0 +1,35 @@
+
+public class TwoSumArray {
+	
+	static boolean twoSum (int[] arr, int target) {
+		int n = arr.length;
+		
+		for(int i = 0; i < n; i++) {
+			
+			for(int j = i + 1; j < n; j++) {
+				
+				if(arr[i] + arr[j] == target)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = { 0, 1, -1, 3, 2, 0
+	};
+		int target = -2;
+		
+		if(twoSum(arr, target))
+			
+			System.out.println("true");
+		else
+			System.out.println("false");
+
+}
+	
+}
