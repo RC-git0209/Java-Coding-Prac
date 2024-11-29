8fc3707c391f7b340e5f7e54753225869fe96a90
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-11-29 15:32:34 +0530

    Made some changes in code

--- src/TwoSumArray.java
+++ src/TwoSumArray.java
@@ -20,7 +20,7 @@ public class TwoSumArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[] = { 0, 1, -1, 3, 2, 0
+		int arr[] = { 0, 1, -3, 3, 2, 0
 	};
 		int target = -2;
 		
