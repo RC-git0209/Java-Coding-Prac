3b2df5c550c1d0d351cdefc4090aa4afd30191e3
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-12-02 16:15:02 +0530

    Final Commit for Duplicate Number

--- src/FindDuplicate.java
+++ src/FindDuplicate.java
@@ -1,27 +1,26 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class FindDuplicate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+		int [] nums = {4, 3, 2, 7, 8, 2, 3, 1, 4};
 		
-		List<Integer> duplicates = findDuplicates(nums);
+		List<Integer> duplicate = findDuplicates(nums);
 		
-		System.out.println("Duplicates: " + duplicates);
+		System.out.println("Duplicates: " + duplicate);
 	}
 	
 	public static List<Integer> findDuplicates(int[] nums) {
 		Set<Integer> seen = new HashSet<>();
-		Set<Integer> duplicates = new HashSet<>();
+		Set<Integer> duplicate = new HashSet<>();
 		
 		for(int num : nums) {
 			if(!seen.add(num)) {
-				duplicates.add(num);
+				duplicate.add(num);
 			}
 		}
-		return new ArrayList<>(duplicates);
+		return new ArrayList<>(duplicate);
 	}
 
 }
