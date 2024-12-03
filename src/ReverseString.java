876efb6a149c4f99383ec06b9aa0afc2b5f25161
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-12-03 13:11:09 +0530

    Made some changes in the Code

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -2,16 +2,25 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Scanner;
 
 public class ReverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "My Name is Rohan, I am a Java Fullstack Developer";
-		String reverse = reverseSentence(s);
-		System.out.println("Reveresed sentence: \"" + reverse + "\"");
-
-	}
+		//String s = "My Name is Rohan, I am a Java Fullstack Developer";
+		//String reverse = reverseSentence(s);
+		//System.out.println("Reveresed sentence: \"" + reverse + "\"");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the String: ");
+		String s = sc.nextLine();
+		String o = reverseSentence(s);
+		
+		//System.out.println("Input String: " + s);
+		System.out.println("Output String: " + o); 
+		
+		}
 	
 	public static String reverseSentence(String sentence) {
 		if (sentence == null || sentence.isEmpty()) {
