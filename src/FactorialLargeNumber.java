64de8b6b279a77eb71b8d2423e9b619d06ef8614
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-12-04 14:12:05 +0530

    Made some changes in code to check with different inputs

--- src/FactorialLargeNumber.java
+++ src/FactorialLargeNumber.java
@@ -3,7 +3,7 @@ public class FactorialLargeNumber {
 	public static int trailingZeroes(int n) {
 	int count = 0;
 	while(n>0) {
-		n/=5;
+		n/=2;
 		count += n;
 	}
 	return count;
@@ -11,6 +11,8 @@ public class FactorialLargeNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Input: n = 3, Output: " + trailingZeroes(3));
+		System.out.println("Input: n = 5, Output: " + trailingZeroes(5));
+		System.out.println("Input: n = 10, Output: " + trailingZeroes(15));
 	}
 
 }
