8b67025ccd984f5a278aba7d6f9c9459f8c724dc
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-12-04 14:04:36 +0530

    Initial commit for Factorial of the Largest number

---
+++ src/FactorialLargeNumber.java
@@ -0,0 +1,16 @@
+
+public class FactorialLargeNumber {
+	public static int trailingZeroes(int n) {
+	int count = 0;
+	while(n>0) {
+		n/=5;
+		count += n;
+	}
+	return count;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Input: n = 3, Output: " + trailingZeroes(3));
+	}
+
+}
