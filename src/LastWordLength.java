9ea044ced65d61531650b351209ea3992937026a
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-12-18 16:58:23 +0530

    Initial commit of Last Word Length

---
+++ src/LastWordLength.java
@@ -0,0 +1,28 @@
+
+public class LastWordLength {
+	public static int lengthOfLastWord(String s) {
+        // Trim any trailing or leading spaces
+        s = s.trim();
+        
+        // Find the last occurrence of a space
+        int lastSpaceIndex = s.lastIndexOf(' ');
+        
+        // Return the length of the last word
+        return s.length() - lastSpaceIndex - 1;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Hello World";
+        System.out.println("Output: " + lengthOfLastWord(s1)); 
+
+        
+        String s2 = "   fly me   to   the moon  ";
+        System.out.println("Output: " + lengthOfLastWord(s2)); 
+
+        
+        String s3 = "luffy is still joyboy";
+        System.out.println("Output: " + lengthOfLastWord(s3)); 
+	}
+
+}
