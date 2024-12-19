8e811506df05219d30372d3cc0fdc38851b0f603
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-12-19 17:56:11 +0530

    Initial commit to remove duplicates

---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+	public static String removeDuplicates(String s) {
+		Set<Character> seen = new LinkedHashSet<>();
+		StringBuilder result = new StringBuilder();
+		
+		for (char c : s.toCharArray()) {
+			if(seen.add(c)) {
+				result.append(c);
+			}
+		}
+		
+		return result.toString();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "applemangotang";
+		String result = removeDuplicates(s);
+		System.out.println("Original String: " + s);
+		System.out.println("String after removing Duplicates: " + result);
+	}
+
+}
