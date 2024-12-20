78619deaaaa75b4a62c198d38155a2750eae0dc8
Rohan Choudhary <rchoudhary@griddynamics.com>
2024-12-20 11:56:11 +0530

    Initial commit for the Missing Number

---
+++ src/MissingNumber.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingNumber {
+	public static List<Integer> findMissingNumbers(int[] arr, int start, int end) {
+		List<Integer> missingNumbers = new ArrayList<>();
+		
+		int expected = start;
+		for(int num : arr) {
+			while(expected < num) {
+				missingNumbers.add(expected);
+				expected++;
+			}
+			expected = num + 1;
+		}
+		while(expected <= end) {
+			missingNumbers.add(expected);
+			expected++;
+		}
+		
+		return missingNumbers;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 2, 4, 6, 7, 10}; //Sorted Array
+		int start = 1; //Range start
+		int end = 10; // Range end
+		
+		List<Integer> missingNumbers = findMissingNumbers(arr, start, end);
+		System.out.println("Missing numbers: " + missingNumbers);
+}
+}
