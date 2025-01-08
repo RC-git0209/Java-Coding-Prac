381a9bcd3a2927258ea8fc80e295e2d94eeb9792
Rohan Choudhary <rchoudhary@griddynamics.com>
2025-01-08 17:30:56 +0530

    Initial commit for the Array Operations

---
+++ src/ArrayOperations.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ArrayOperations {
+
+	public static void main(String[] args) {
+		int[] arr = {2,5,3,6,7,11,9};
+		
+		performOperations(arr);
+		
+		Arrays.sort(arr);
+		
+		System.out.println("Array after sorting: " + Arrays.toString(arr));
+		
+		performOperations(arr);
+
+	}
+	
+	public static void performOperations(int[] arr) 
+	{
+		int min = Arrays.stream(arr).min().orElseThrow();
+		int max = Arrays.stream(arr).max().orElseThrow();
+		int sum = Arrays.stream(arr).sum();
+		double avg = Arrays.stream(arr).average().orElseThrow();
+		
+		System.out.println("min: " + min);
+		System.out.println("max: " + max);
+		System.out.println("sum: " + sum);
+		System.out.println("avg: " + avg);
+	}
+
+}
