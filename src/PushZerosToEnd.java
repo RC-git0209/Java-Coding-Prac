5ca3bc9450ade4b0af8b46d0e795f4955a0ad3ed
Rohan Choudhary <rchoudhary@griddynamics.com>
2025-01-20 17:06:25 +0530

    Final commit to push zeros to the end

--- src/PushZerosToEnd.java
+++ src/PushZerosToEnd.java
@@ -1,20 +1,27 @@
 import java.util.Arrays;
 
 public class PushZerosToEnd {
+	
+	//function to move all zeros to the end
 	static void pushZerosToEnd(int [] arr) {
 		int n = arr.length;
 		int [] temp = new int[n];
 		
+		// to keep track of the index in temp[]
 		int j = 0;
 		
+		//copy non-zeros elements to temp[]
 		for(int i = 0; i < n; i++) {
 			if(arr[i] != 0)
 				temp[j++] = arr[i];
 		}
 		
-		while (j < n)
+		
+		//Fill remaining positions in temp[] with zeros
+ 		while (j < n)
 			temp[j++] = 0;
 		
+ 		//copy all the elements from temp[] to arr[]
 		for (int i = 0; i < n; i++)
 			arr[i] = temp[i];
 	}
@@ -23,6 +30,7 @@ public class PushZerosToEnd {
 		int[] arr = {1, 2, 0, 5, 0 , 6, 0, 7, 9};
 		pushZerosToEnd(arr);
 		
+		//print the modified array
 		for(int num : arr) {
 			System.out.print(num + " ");
 		}
